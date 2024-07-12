d22b5a892caf0f6acdede05dc3ef099bea2e6cf7
DemandingSloth <bamberg.tucker@gmail.com>
2024-07-12 17:26:31 -0400

    Update chainreport.py
    Added username, lowered required hits to 3

--- chainreport.py
+++ chainreport.py
@@ -2,6 +2,7 @@ import os
 from dotenv import load_dotenv
 import requests
 import json
+import time  
 
 load_dotenv()
 
@@ -10,12 +11,11 @@ TOKEN = os.getenv("TORN_KEY")
 timefrom = int(input("When from? "))
 timeto = int(input("When to? "))
 
-# get chain info!
 print("Getting chain info...")
 chains = requests.get(f"https://api.torn.com/faction/?selections=chains&key={TOKEN}&from={timefrom}&to={timeto}")
 chains = json.loads(chains.content)["chains"]
 print(f"Loaded {len(chains)} chains")
-
+time.sleep(.1)
 contribs = {}
 
 for chainid in chains:
@@ -24,10 +24,19 @@ for chainid in chains:
     info = json.loads(info.content)["chainreport"]
     members = info["members"]
 
-    for m, mdata in members.items():
-        if mdata["attacks"] >= 4: # if enough hits
-            if m not in contribs.keys():
-                contribs[m] = mdata["attacks"] # if not in dict, add member
-            else: contribs[m] += mdata["attacks"] # add number of hits
+     
 
-print(contribs)
+    for member_id, mdata in members.items():
+        if mdata["attacks"] >= 3:  
+            if member_id not in contribs.keys():
+                contribs[member_id] = mdata["attacks"]
+            else:
+                contribs[member_id] += mdata["attacks"]
+
+# Get member profiles and print results
+print("Player ID", "Name", "Attacks") 
+for member_id, attacks in contribs.items():
+    profileInfo = requests.get(f"https://api.torn.com/user/{member_id}/?key={TOKEN}")  # Removed selections=profile
+    profileInfo = json.loads(profileInfo.content)
+    name = profileInfo["name"] 
+    print(member_id, name, attacks) 
